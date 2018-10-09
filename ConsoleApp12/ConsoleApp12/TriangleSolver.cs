625f4650d39004e2c8012f44a28c92ddf4104b9b
gbabbar101 <43212641+gbabbar101@users.noreply.github.com>
2018-10-09 00:05:53 -0400

    Add files via upload

---
+++ ConsoleApp12/ConsoleApp12/TriangleSolver.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    public class TriangleSolver
+    {
+            int x, y, z;
+            public TriangleSolver()
+            {
+            }
+        public string Analyze(int x, int y, int z)
+        {
+            if (x <= 0 || y <= 0 || z <= 0)
+            {
+                Console.WriteLine("Invalid Value");
+            }
+            else if (((x + y) > z) && ((y + z) > x) && ((z + x) > y))
+            {
+                Console.WriteLine("Triangle:");
+                if (x == y && y == z && z == x)
+                {
+                    Console.WriteLine("The triangle is an Equilateral Triangle");
+                    return ("This is a Equilateral triangle");
+                }
+                else if (x == y || y == z || z == x)
+                {
+                    Console.WriteLine("The triangle is an Isosceles Triangle");
+                    return ("This is a Isosceles triangle");
+
+                }
+                else if (x != y && y != z && z != x)
+                {
+                    Console.WriteLine("The triangle is an Scalene Triangle");
+                    return ("This is a Scalene triangle");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Enter valid values again");
+            }
+            return ("InvalidEntry");
+
+        }
+        //throw new NotImplementedException();
+    }
+    }
+
---
+++ ConsoleApp12/test_cases/Triangle_test_cases.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp12;
+using NUnit.Framework;
+
+namespace test_cases
+{
+    public class Triangle_test_cases
+    {
+        public class TriangleTestCases
+        {
+            [Test]
+            public void Analyze_Input5and5and5_resultIsEquilateral()
+            {
+                //Arrange
+                int value1 = 5;
+                int value2 = 5;
+                int value3 = 5;
+                string result = "This is a Equilateral triangle";
+                TriangleSolver triangle= new TriangleSolver();
+
+                //Act
+                string action = triangle.Analyze(value1, value2, value3);
+
+                //Assert
+                Assert.AreEqual(result, action);
+            }
+
+            [Test]
+            public void Analyze_Input5and5and8_resultIsIsosceles()
+            {
+                //Arrange
+                int value1 = 5;
+                int value2 = 5;
+                int value3 = 8;
+                string result = "This is a Isosceles triangle";
+                TriangleSolver triangle = new TriangleSolver();
+
+                //Act
+                string action = triangle.Analyze(value1, value2, value3);
+
+                //Assert
+                Assert.AreEqual(result, action);
+            }
+
+            [Test]
+            public void Analyze_Input4and7and9_resultIsScalene()
+            {
+                //Arrange
+                int value1 = 4;
+                int value2 = 7;
+                int value3 = 9;
+                string result = "This is a Scalene triangle";
+                TriangleSolver triangle = new TriangleSolver();
+
+                //Act
+                string action = triangle.Analyze(value1, value2, value3);
+
+                //Assert
+                Assert.AreEqual(result, action);
+            }
+
+            [Test]
+            public void Analyze_Input123and100and1_InvalidEntry()
+            {
+                //Arrange
+                int value1 = 123;
+                int value2 = 100;
+                int value3 = 1;
+                string result = "InvalidEntry";
+                TriangleSolver triangle = new TriangleSolver();
+
+                //Act
+                string action = triangle.Analyze(value1, value2, value3);
+
+                //Assert
+                Assert.AreEqual(result, action);
+            }
+
+            [Test]
+            public void Analyze_Input1and5and0_InvalidEntry()
+            {
+                //Arrange
+                int value1 = 1;
+                int value2 = 5;
+                int value3 = 0;
+                string result = "InvalidEntry";
+                TriangleSolver triangle = new TriangleSolver();
+
+                //Act
+                string action = triangle.Analyze(value1, value2, value3);
+
+                //Assert
+                Assert.AreEqual(result, action);
+            }
+            [Test]
+            public void Analyze_Inputnegative1andnegative5and0_InvalidEntry()
+            {
+                //Arrange
+                int value1 = -1;
+                int value2 = -5;
+                int value3 = 0;
+                string result = "InvalidEntry";
+                TriangleSolver triangle = new TriangleSolver();
+
+                //Act
+                string action = triangle.Analyze(value1, value2, value3);
+
+                //Assert
+                Assert.AreEqual(result, action);
+            }
+            [Test]
+            public void Analyze_Input0and0and0_InvalidEntry()
+            {
+                //Arrange
+                int value1 = 0;
+                int value2 = 0;
+                int value3 = 0;
+                string result = "InvalidEntry";
+                TriangleSolver triangle = new TriangleSolver();
+
+                //Act
+                string action = triangle.Analyze(value1, value2, value3);
+
+                //Assert
+                Assert.AreEqual(result, action);
+            }
+            [Test]
+            public void Analyze_Inputnegative1andnegative2andnegative3_InvalidEntry()
+            {
+                //Arrange
+                int value1 = -1;
+                int value2 = -2;
+                int value3 = -3;
+                string result = "InvalidEntry";
+                TriangleSolver triangle = new TriangleSolver();
+
+                //Act
+                string action = triangle.Analyze(value1, value2, value3);
+
+                //Assert
+                Assert.AreEqual(result, action);
+            }
+        }
+    }
+}
