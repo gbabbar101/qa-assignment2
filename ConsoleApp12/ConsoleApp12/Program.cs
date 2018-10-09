62a1af581bf4e58f6883e29d91630c54d7667efd
gbabbar101 <43212641+gbabbar101@users.noreply.github.com>
2018-10-09 00:08:19 -0400

    Update Program.cs

--- ConsoleApp12/ConsoleApp12/Program.cs
+++ ConsoleApp12/ConsoleApp12/Program.cs
@@ -6,11 +6,65 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp12
 {
-    class Program    {        static void Main(string[] args)        {            TriangleSolver t = new TriangleSolver();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TriangleSolver t = new TriangleSolver();
 
-            int aValue, bValue, cValue;            string a, b, c;
-            string InputValue = string.Empty;            bool validMenuSelection = false;            try            {                while (validMenuSelection == false)                {                    Console.WriteLine("Press 1: Enter Dimensions of a Triangle");                    Console.WriteLine("Press 2: Exit");                    InputValue = Console.ReadLine();                    if (InputValue == "1")                    {                        Console.WriteLine("Enter 3 values as sides of Triangle");                        a = Console.ReadLine();                        bool a1 = int.TryParse(a, out aValue);                        if (!(a1))                        {                            Console.WriteLine("The entered value is not valid");                        }                        b = Console.ReadLine();                        bool b1 = int.TryParse(b, out bValue);                        if (!(b1))                        {                            Console.WriteLine("The entered value is not valid");                        }                        c = Console.ReadLine();                        bool c1 = int.TryParse(c, out cValue);                        if (!(c1))                        {                            Console.WriteLine("The entered value is  not valid");                        }
+            int aValue, bValue, cValue;
+            string a, b, c;
+            string InputValue = string.Empty;
+
+
+            bool validMenuSelection = false;
+           
+                while (validMenuSelection == false)
+                {
+                    Console.WriteLine("Press 1: Enter Dimensions of a Triangle");
+                    Console.WriteLine("Press 2: Exit");
+
+                    InputValue = Console.ReadLine();
+
+                    if (InputValue == "1")
+                    {
+                        Console.WriteLine("Enter 3 values as sides of Triangle");
+
+                        a = Console.ReadLine();
+                        bool a1 = int.TryParse(a, out aValue);
+                        if (!(a1))
+                        {
+                            Console.WriteLine("The entered value is not valid");
+                        }
+
+                        b = Console.ReadLine();
+                        bool b1 = int.TryParse(b, out bValue);
+                        if (!(b1))
+                        {
+                            Console.WriteLine("The entered value is not valid");
+                        }
+
+                        c = Console.ReadLine();
+                        bool c1 = int.TryParse(c, out cValue);
+                        if (!(c1))
+                        {
+                            Console.WriteLine("The entered value is  not valid");
+                        }
 
                         t.Analyze(aValue, bValue, cValue);
 
-                    }                    else if (InputValue == "2")                    {                        System.Environment.Exit(0);                    }                    else                    {                        Console.WriteLine("Please enter a valid value");                    }                }            }            catch (Exception e)            {                Console.WriteLine(e.Message);            }        }    }}
+                    }
+                    else if (InputValue == "2")
+                    {
+                        System.Environment.Exit(0);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter a valid value");
+                    }
+                }
+            }
+            
+        }
+    }
+}
